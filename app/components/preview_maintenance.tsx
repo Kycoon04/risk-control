16655ae5b65662375e21fd616c2d3a202be4ac24
Kycoon <jomaval4@gmail.com>
2024-04-13 11:44:21 -0600

    [REFACTOR] maintenance page

--- app/components/preview_maintenance.tsx
+++ app/components/preview_maintenance.tsx
@@ -1,28 +1,39 @@
 "use client";
 import Link from 'next/link';
-import { FaPen } from "react-icons/fa6";
+import { RiUserSettingsFill } from "react-icons/ri";
+import { SiGoogleforms } from "react-icons/si";
+import { BsFillQuestionSquareFill } from "react-icons/bs";
+import { SiGooglemarketingplatform } from "react-icons/si";
 
 interface Field {
     titule: string;
-    url:string;
+    url: string;
+    icon: 'RiUserSettingsFill' | 'SiGoogleforms' | 'BsFillQuestionSquareFill' | 'SiGooglemarketingplatform';
 }
 
-const Componente: React.FC<Field> = ({ titule,url}) => {
+const PreviewMaintenance: React.FC<Field> = ({ titule, url, icon }) => {
+
+    const iconComponent =
+        icon === 'RiUserSettingsFill' ? <RiUserSettingsFill className='text-4xl m-4' /> :
+        icon === 'SiGoogleforms' ? <SiGoogleforms className='text-4xl m-4' /> :
+        icon === 'BsFillQuestionSquareFill' ? <BsFillQuestionSquareFill className='text-4xl m-4' /> :
+        icon === 'SiGooglemarketingplatform' ? <SiGooglemarketingplatform className='text-4xl m-4' /> :
+        null;
+
     return (
-        <div className='flex justify-center'>
-            <div className='flex flex-col sm:flex-row bg-gray-200 m-3 p-3 rounded-3xl gap-10 items-center justify-start'>
-                <h2 className='text-2xl sm:pr-10 text-center sm:text-left text-white'>
+        <div className='cursor-pointer'>
+            <div className='bg-gray-200 m-3 p-3 flex flex-col rounded-3xl items-center justify-center hover:scale-105'>
+                <h2 className='text-2xl sm:text-center text-white text-center m-5'>
                     {titule}
                 </h2>
-                <div className='bg-purple-400 rounded-3xl text-white cursor-pointer'>
+                <div className='bg-purple-400 flex gap-5 rounded-3xl text-white cursor-pointer justify-center'>
                     <Link href={url}>
-                        <FaPen className='text-4xl m-4' />
+                    {iconComponent}
                     </Link>
                 </div>
             </div>
         </div>
-
     );
-}
+};
 
-export default Componente;
+export default PreviewMaintenance;
