a76585d340ca0c688a76f25a3996d89800c8bac9
Kycoon <jomaval4@gmail.com>
2024-05-12 12:12:53 -0600

    [FIX] rol by user main

--- app/home_page/forms/page.tsx
+++ app/home_page/forms/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   const rol = useAuthStore(state => state.rol);
   return (
     <main className="flex min-h-screen flex-col items-center bg-gradient-radial rounded-lg from-purple-500 via-purple-700 to-purple-1000">
-      {rol === "Administrador" ? <Admin_global /> : null}
+      {rol?.some(role => role.name === "Admi senior") ? <Admin_global /> : null}
       <About_forms/>
     </main>
   );
