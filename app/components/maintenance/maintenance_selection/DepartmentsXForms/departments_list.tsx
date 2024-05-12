7534b7fff3ebd53faa73e01d01274dfca730970c
Anderson Fernández <anderfb1203@gmail.com>
2024-05-12 10:56:50 -0600

    [REFACTOR] selection DepartmentXForm

--- app/components/maintenance/maintenance_selection/DepartmentsXForms/departments_list.tsx
+++ app/components/maintenance/maintenance_selection/DepartmentsXForms/departments_list.tsx
@@ -1,18 +1,14 @@
 "use client";
 import Link from 'next/link';
-import { FaRegPlusSquare } from "react-icons/fa";
 import { ParamDepartment,departXForms,departmentSelected } from "@/types";
 import { useState, useEffect } from "react";
-import {fetchDepartment} from "../../../actions/actions_departments/actions";
-import { fetchDepartXIdForms } from '@/app/components/actions/actions_deparxforms/actions';
 import Spinner from "../../../notifications/Spinner";
 import Filter from "../../../utils_comp/Filters/filter";
 import DepartmentCard from "../../../maintenance/maintenance_selection/DepartmentsXForms/department_card";
-import { Success, Error } from "../../../notifications/alerts";
 import { useAuthStore } from '@/app/components/maintenance/maintenance_storages/form_storage';
 import {deleteDepartXIdForms, postDepartXForm} from "../../../actions/actions_deparxforms/actions";
 import { IoReturnDownBack } from "react-icons/io5";
-import {param,importList} from '../DepartmentsXForms/selection_methods'
+import {param,importList,filtered,stateAdded,stateDeleted,updateData,loadData} from '../DepartmentsXForms/selection_methods'
 const DepartmentsList: React.FC = () => {
     const [departments,setDepartments] =useState<ParamDepartment[]>([]);
     const [departmentsState,setDepartmentsState] =useState<departmentSelected[]>([]);
@@ -21,78 +17,25 @@ const DepartmentsList: React.FC = () => {
     const [unfiltered, setUnfiltered] = useState<ParamDepartment[]>([]);
     const [filters, setFilters] = useState<Partial<ParamDepartment>>(param);
     const form = useAuthStore(state => state.form);
-    const clearFilters = () => {
-        setFilters(param);
-        setDepartments(unfiltered);
-    };
+    const clearFilters = () => { setFilters(param); setDepartments(unfiltered); };
     useEffect(() => {
-        const fetchData = async () => {
-            setIsLoading(true);
-            const fetchedSections = await fetchDepartment(param);
-            setDepartments(fetchedSections.props.data);
-            const fetchedDepartXForm = await fetchDepartXIdForms(form.id);
-            console.log(fetchedDepartXForm)
-            setDepartXForms(fetchedDepartXForm);
-            setUnfiltered(fetchedSections.props.data);
-            setIsLoading(false);
-        };
-        fetchData();
+        const fetchData = async () => { loadData(setDepartments,setUnfiltered,setDepartXForms,form, setIsLoading); }; fetchData();
     }, []);
-
     useEffect(() => {
         const fetchData = async () => {
-            const list = importList(departXForm, departments);
-            setDepartmentsState(list);
-            console.log(departmentsState)
-        };
-        if (departments.length > 0 && departXForm.length > 0) {fetchData();}
+            setDepartmentsState(importList(departXForm, departments));
+        }; if (departments.length > 0 && departXForm.length > 0) {fetchData();}
     }, [departments, departXForm]);
     useEffect(() => {
-        const applyFilters = () => {
-            const filteredDepartments = unfiltered.filter(item => {
-                return Object.keys(filters).every(key => {
-                    const filterValue = filters[key as keyof ParamDepartment];
-                    const itemValue = item[key as keyof ParamDepartment];
-
-                    if (typeof filterValue === 'number' || typeof itemValue === 'number') {
-                        const stringValue = String(filterValue).toLowerCase();
-                        const itemStringValue = String(itemValue).toLowerCase();
-                        return itemStringValue.includes(stringValue);
-                    } else {
-                        return itemValue.toLowerCase().includes((filterValue || "").toLowerCase());
-                    }
-                });
-            });
-            setDepartments( filteredDepartments);
-        };
-        applyFilters();
+        const applyFilters = () => { setDepartments( filtered(unfiltered,filters)); }; applyFilters();
     }, [filters, unfiltered]);
-
     const handleDeleteDepartment = async (departmentId: string) => {
         const deletionResult = await deleteDepartXIdForms(parseInt(form.id, 10),parseInt(departmentId, 10));
-        if (deletionResult) {
-            Success('Departamento eliminado correctamente')
-            const fetchedSections = await fetchDepartment(param);
-            setDepartments(fetchedSections.props.data);
-            setUnfiltered(fetchedSections.props.data);
-            const fetchedDepartXForm = await fetchDepartXIdForms(form.id);
-            setDepartXForms(fetchedDepartXForm);
-        } else {
-            Error('Error al intentar eliminar el departamento');
-        }
+        stateDeleted(deletionResult,setDepartments,setUnfiltered,setDepartXForms,form);
     };
     const handleAddDepartment = async (departmentId: string) => {
-        const deletionResult = await postDepartXForm(parseInt(form.id, 10),parseInt(departmentId, 10));
-        if (deletionResult) {
-            Success('Departamento agregado correctamente')
-            const fetchedSections = await fetchDepartment(param);
-            setDepartments(fetchedSections.props.data);
-            setUnfiltered(fetchedSections.props.data);
-            const fetchedDepartXForm = await fetchDepartXIdForms(form.id);
-            setDepartXForms(fetchedDepartXForm);
-        } else {
-            Error('Error al intentar agregar el departamento');
-        }
+        const addedResult = await postDepartXForm(parseInt(form.id, 10),parseInt(departmentId, 10));
+        stateAdded(addedResult?.ok||false,setDepartments,setUnfiltered,setDepartXForms,form);
     };
     return (
         <div className='bg-gray-200 w-90vw md:w-90 sm:w-[90%] m-3 p-3 flex flex-col rounded-3xl items-center justify-center'>
--- app/components/maintenance/maintenance_selection/DepartmentsXForms/selection_methods.ts
+++ app/components/maintenance/maintenance_selection/DepartmentsXForms/selection_methods.ts
@@ -1,26 +1,59 @@
 "use client";
-import { ParamDepartment,departXForms,departmentSelected } from "@/types";
+import { ParamDepartment,departXForms,departmentSelected,Form } from "@/types";
 import {fetchDepartment} from "../../../actions/actions_departments/actions";
 import { fetchDepartXIdForms } from '@/app/components/actions/actions_deparxforms/actions';
 import { Success, Error } from "../../../notifications/alerts";
-import { useAuthStore } from '@/app/components/maintenance/maintenance_storages/form_storage';
-import {deleteDepartXIdForms, postDepartXForm} from "../../../actions/actions_deparxforms/actions";
-import { list } from "postcss";
-export const param: ParamDepartment = {
-    id: "",
-    name: "",
-    description: "",
-    unit: "",
-};
-
+import { Dispatch, SetStateAction } from "react";
+export const param: ParamDepartment = { id: "", name: "", description: "", unit: "", };
+export const loadData = (setDepartments:Dispatch<SetStateAction<ParamDepartment[]>>,setUnfiltered:Dispatch<SetStateAction<ParamDepartment[]>>,setDepartXForms:Dispatch<SetStateAction<departXForms[]>>,form:Form,setIsLoading:Dispatch<SetStateAction<boolean>>) =>{
+    setIsLoading(true);
+    updateData(setDepartments,setUnfiltered,setDepartXForms,form);
+    setIsLoading(false);
+}
 export const importList = (departXForm: departXForms[], departments: ParamDepartment[]): departmentSelected[] => {
         const list: departmentSelected[] = [];
             for (let i = 0; i < departments.length; i++) {
                 const isDepartmentSelected = departXForm.some(depForm => depForm.department === departments[i].id);
-                list.push({
-                    department: departments[i],
-                    state: isDepartmentSelected ? "Agregado" : "No Agregado",
-                });
+                list.push({ department: departments[i], state: isDepartmentSelected ? "Agregado" : "No Agregado", });
             }
         return list;
+}
+export const filtered  = (unfiltered:ParamDepartment[],filters:Partial<ParamDepartment>) => {
+    const filteredDepartments = unfiltered.filter(item => {
+        return Object.keys(filters).every(key => {
+            const filterValue = filters[key as keyof ParamDepartment];
+            const itemValue = item[key as keyof ParamDepartment];
+            if (typeof filterValue === 'number' || typeof itemValue === 'number') {
+                const stringValue = String(filterValue).toLowerCase();
+                const itemStringValue = String(itemValue).toLowerCase();
+                return itemStringValue.includes(stringValue);
+            } else {
+                return itemValue.toLowerCase().includes((filterValue || "").toLowerCase());
+            }
+        });
+    });
+    return filteredDepartments;
+}
+export const stateDeleted = async (deletionResult:boolean,setDepartments:Dispatch<SetStateAction<ParamDepartment[]>>,setUnfiltered:Dispatch<SetStateAction<ParamDepartment[]>>,setDepartXForms:Dispatch<SetStateAction<departXForms[]>>,form:Form) => {
+    if (deletionResult) {
+        Success('Departamento eliminado correctamente')
+        updateData(setDepartments,setUnfiltered,setDepartXForms,form);
+    } else {
+        Error('Error al intentar eliminar el departamento');
+    }
+}
+export const stateAdded = async (AddedResult:boolean,setDepartments:Dispatch<SetStateAction<ParamDepartment[]>>,setUnfiltered:Dispatch<SetStateAction<ParamDepartment[]>>,setDepartXForms:Dispatch<SetStateAction<departXForms[]>>,form:Form) => {
+    if (AddedResult) {
+        Success('Departamento agregado correctamente')
+        updateData(setDepartments,setUnfiltered,setDepartXForms,form);
+    } else {
+        Error('Error al intentar agregar el departamento');
+    }
+}
+export const updateData = async (setDepartments:Dispatch<SetStateAction<ParamDepartment[]>>,setUnfiltered:Dispatch<SetStateAction<ParamDepartment[]>>,setDepartXForms:Dispatch<SetStateAction<departXForms[]>>,form:Form) => {
+    const fetchedSections = await fetchDepartment(param);
+    setDepartments(fetchedSections.props.data);
+    setUnfiltered(fetchedSections.props.data);
+    const fetchedDepartXForm = await fetchDepartXIdForms(form.id);
+    setDepartXForms(fetchedDepartXForm);
 }
