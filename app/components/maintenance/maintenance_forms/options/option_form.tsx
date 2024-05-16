90943449cb8fc4e52548bf13f5155b64f526c821
Anderson Fernández <anderfb1203@gmail.com>
2024-05-15 22:10:44 -0600

    [ADD] select options scores

--- app/components/maintenance/maintenance_forms/options/option_form.tsx
+++ app/components/maintenance/maintenance_forms/options/option_form.tsx
@@ -5,12 +5,13 @@ import Field from '@/app/components/utils_forms/Field';
 import Field_Disabled from '@/app/components/utils_forms/Field_Disabled';
 import Text_Area from '@/app/components/utils_forms/Text_Area';
 import Standard_button from '@/app/components/utils_forms/Button';
-import { makeChange } from '@/lib/validation/makeChange';
+import { changeState } from '@/lib/validation/changeState';
 import { fetchQuestion} from '@/app/components/actions/actions_questions/actions'
 import { useAuthStore } from '@/app/components/maintenance/maintenance_storages/option_storage';
 import { ParamOption } from '@/types';
 import ChoiseBox from '@/app/components/register/selectQuestion';
 import { submitFormOption, Questions, comeBack} from '../options/maintenance_methods';
+import ChoiseBoxScore from '@/app/components/utils_forms/ChoiseBox_States'
 
 const Option_Form: React.FC = () => {
      const Option = useAuthStore(state => state.option);
@@ -20,7 +21,8 @@ const Option_Form: React.FC = () => {
     const [option, setOption] = useState(Option?.option);
     const [questions, setQuestions] = useState<ParamOption[]>([]);
     const [score, setScore] = useState(Option?.score);
-    const { handleSubmit, register, formState: { errors } } = makeChange();
+    const [scores, setScores] = useState<string[]>(['20', '40','60','80','100']);
+    const { handleSubmit, register, formState: { errors } } = changeState();
     useEffect(() => {
         if (Option) { setLoading(false); }
     }, [Option]);
@@ -39,8 +41,8 @@ const Option_Form: React.FC = () => {
         <div className=' py-5 drop-shadow-lg m-1 flex flex-col items-center pr-7 pl-7' >
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4 my-4 w-full">
                 <Field_Disabled text_Field={id} setText_Field={setId} titule={'Id:'} type={"text"} register={register} error={errors.id} name={"id"}></Field_Disabled>
-                <Text_Area text_Field={option} setText_Field={setOption} titule={'Opción:'} type={"text"} register={register} error={errors.description} name={"description"}></Text_Area>
-                <Field_Disabled text_Field={score} setText_Field={setScore} titule={'Puntaje:'} type={"text"} register={register} error={errors.name} name={"name"}></Field_Disabled>
+                <Text_Area text_Field={option} setText_Field={setOption} titule={'Opción:'} type={"text"} register={register} error={errors.name} name={"name"}></Text_Area>
+                <ChoiseBoxScore data={scores} selectData={score} onChange={setScore} titule="Puntaje:"></ChoiseBoxScore>
                 <ChoiseBox data={questions} selectData={question} onChange={setQuestion} titule="Preguntas:"/>
             </div>
             <div className='grid grid-cols-2 md:grid-cols-2 gap-8 justify-center'>
--- app/components/register/register_pages/options/option_form.tsx
+++ app/components/register/register_pages/options/option_form.tsx
@@ -10,6 +10,7 @@ import { fetchQuestion} from '@/app/components/actions/actions_questions/actions
 import { useAuthStore } from '@/app/components/maintenance/maintenance_storages/option_storage';
 import { ParamOption } from '@/types';
 import ChoiseBox from '@/app/components/register/selectQuestion';
+import ChoiseBoxScore from '@/app/components/utils_forms/ChoiseBox_States'
 import { submitFormOption, Questions} from '../options/register_methods';
 
 const Option_Form: React.FC = () => {
@@ -19,6 +20,7 @@ const Option_Form: React.FC = () => {
     const [question, setQuestion] = useState("1");
     const [option, setOption] = useState("");
     const [questions, setQuestions] = useState<ParamOption[]>([]);
+    const [scores, setScores] = useState<string[]>(['20', '40','60','80','100']);
     const [score, setScore] = useState("");
     const { handleSubmit, register, formState: { errors } } = makeValidationForm();
     useEffect(() => {
@@ -32,7 +34,7 @@ const Option_Form: React.FC = () => {
         initialize();
     }, []);
     const submitForm = async () => {
-        submitFormOption(option,question,"0");
+        submitFormOption(option,question,score);
     }
     const comeBack = async () => { }
     return (
@@ -41,6 +43,7 @@ const Option_Form: React.FC = () => {
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4 my-4 w-full">
                 <Text_Area text_Field={option} setText_Field={setOption} titule={'Opción:'} type={"text"} register={register} error={errors.name} name={"name"}></Text_Area>
                 <ChoiseBox data={questions} selectData={question} onChange={setQuestion} titule="Preguntas:"/>
+                <ChoiseBoxScore data={scores} selectData={score} onChange={setScore} titule="Puntaje:"></ChoiseBoxScore>
             </div>
             <div className='grid grid-cols-2 md:grid-cols-2 gap-8 justify-center'>
                 <div className='flex justify-center'>
