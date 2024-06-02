6278cd8e406e3f1660520b7d17f3990031619af8
Sebastián Granados Barrantes <117589200+SebastianGranadosBarrantes@users.noreply.github.com>
2024-06-01 21:06:51 -0600

    [DEBUG] vercel error with cron job, test removing middleware

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -103,65 +103,64 @@ async function sendEmailEnd(username : string, department : string, form : strin
             }
         }
 
-export function GET() {
-    return new Response('No forms to start or end today', {status: 200,});
-    // // if (request.headers.get('Authorization') !== `${process.env.CRON_SECRET}`) {
-    // //     return new Response('Unauthorized', {status: 401,});
-    // // }
-    // try {
-    //     let forms : FormCron[] = await GetallForms();
-    //     let departXForms : DepartXFormCron[] = await GetallDepartmentsXForms();
-    //     const users : UserCron[] = await GetallUsers();
-    //     const departments : DepartmentCron[] = await GetallDepartments();
-    //     const today = new Date();
-
-    //     const formsInit = forms.filter((form) => {
-    //         const initialPeriod = new Date(form.inicialperiod);
-    //         return isSameDay(today, initialPeriod);
-    //     });
-
-    //     const formsEnd = forms.filter((form) => {
-    //         const finalPeriod = new Date(form.finalperiod);
-    //         return isSameDay(today, finalPeriod);
-    //     });
-
-    //     if(formsInit.length === 0 && formsEnd.length === 0) {
-    //         return new Response('No forms to start or end today', {status: 200,});
-    //     }
-
-    //     const matchingDepartmentsEnd: DepartXFormCron[] = departXForms.filter((department) => {
-    //         return formsEnd.some(form => form.id === department.forms);
-    //     });
-    //     for (let i = 0; i < matchingDepartmentsEnd.length; i++) {
-    //         matchingDepartmentsEnd[i].department = getDepartmentName(Number(matchingDepartmentsEnd[i].department), departments);
-    //         matchingDepartmentsEnd[i].forms = getFormName(Number(matchingDepartmentsEnd[i].forms), formsEnd);
-    //     }   
-    //     for (let i = 0; i < matchingDepartmentsEnd.length; i++) {
-    //         let usersInDepartment = getUsersInDepartment(users, departments, String(matchingDepartmentsEnd[i].department));
-    //         for (let j = 0; j < usersInDepartment.length; j++) {
-    //             console.log("Sending email to notifiy end :", usersInDepartment[j].name, usersInDepartment[j].email, matchingDepartmentsEnd[i].department, matchingDepartmentsEnd[i].forms);
-    //             await sendEmailEnd(usersInDepartment[j].nickname, String(matchingDepartmentsEnd[i].department), String(matchingDepartmentsEnd[i].forms), usersInDepartment[j].email);
-    //         }
-    //     }
-
-    //     const matchingDepartmentsInit: DepartXFormCron[] = departXForms.filter((department) => {
-    //         return formsInit.some(form => form.id === department.forms);
-    //     });
-    //     for (let i = 0; i < matchingDepartmentsInit.length; i++) {
-    //         matchingDepartmentsInit[i].department = getDepartmentName(Number(matchingDepartmentsInit[i].department), departments);
-    //         matchingDepartmentsInit[i].forms = getFormName(Number(matchingDepartmentsInit[i].forms), formsInit);
-    //     }   
-    //     for (let i = 0; i < matchingDepartmentsInit.length; i++) {
-    //         let usersInDepartment = getUsersInDepartment(users, departments, String(matchingDepartmentsInit[i].department));
-    //         for (let j = 0; j < usersInDepartment.length; j++) {
-    //             console.log("Sending email to notify starting:", usersInDepartment[j].name, usersInDepartment[j].email, matchingDepartmentsInit[i].department, matchingDepartmentsInit[i].forms);
-    //             await sendEmailInit(usersInDepartment[j].nickname, String(matchingDepartmentsInit[i].department), String(matchingDepartmentsInit[i].forms), usersInDepartment[j].email);
-    //         }
-    //     }
-
-    //     return new Response('All the emails were send successfully', {status: 200,});
-    // } catch (error) {
-    //     console.error('Error:', error);
-    //     throw error;
+export async function GET() {
+    // if (request.headers.get('Authorization') !== `${process.env.CRON_SECRET}`) {
+    //     return new Response('Unauthorized', {status: 401,});
     // }
+    try {
+        let forms : FormCron[] = await GetallForms();
+        let departXForms : DepartXFormCron[] = await GetallDepartmentsXForms();
+        const users : UserCron[] = await GetallUsers();
+        const departments : DepartmentCron[] = await GetallDepartments();
+        const today = new Date();
+
+        const formsInit = forms.filter((form) => {
+            const initialPeriod = new Date(form.inicialperiod);
+            return isSameDay(today, initialPeriod);
+        });
+
+        const formsEnd = forms.filter((form) => {
+            const finalPeriod = new Date(form.finalperiod);
+            return isSameDay(today, finalPeriod);
+        });
+
+        if(formsInit.length === 0 && formsEnd.length === 0) {
+            return new Response('No forms to start or end today', {status: 200,});
+        }
+
+        const matchingDepartmentsEnd: DepartXFormCron[] = departXForms.filter((department) => {
+            return formsEnd.some(form => form.id === department.forms);
+        });
+        for (let i = 0; i < matchingDepartmentsEnd.length; i++) {
+            matchingDepartmentsEnd[i].department = getDepartmentName(Number(matchingDepartmentsEnd[i].department), departments);
+            matchingDepartmentsEnd[i].forms = getFormName(Number(matchingDepartmentsEnd[i].forms), formsEnd);
+        }   
+        for (let i = 0; i < matchingDepartmentsEnd.length; i++) {
+            let usersInDepartment = getUsersInDepartment(users, departments, String(matchingDepartmentsEnd[i].department));
+            for (let j = 0; j < usersInDepartment.length; j++) {
+                console.log("Sending email to notifiy end :", usersInDepartment[j].name, usersInDepartment[j].email, matchingDepartmentsEnd[i].department, matchingDepartmentsEnd[i].forms);
+                await sendEmailEnd(usersInDepartment[j].nickname, String(matchingDepartmentsEnd[i].department), String(matchingDepartmentsEnd[i].forms), usersInDepartment[j].email);
+            }
+        }
+
+        const matchingDepartmentsInit: DepartXFormCron[] = departXForms.filter((department) => {
+            return formsInit.some(form => form.id === department.forms);
+        });
+        for (let i = 0; i < matchingDepartmentsInit.length; i++) {
+            matchingDepartmentsInit[i].department = getDepartmentName(Number(matchingDepartmentsInit[i].department), departments);
+            matchingDepartmentsInit[i].forms = getFormName(Number(matchingDepartmentsInit[i].forms), formsInit);
+        }   
+        for (let i = 0; i < matchingDepartmentsInit.length; i++) {
+            let usersInDepartment = getUsersInDepartment(users, departments, String(matchingDepartmentsInit[i].department));
+            for (let j = 0; j < usersInDepartment.length; j++) {
+                console.log("Sending email to notify starting:", usersInDepartment[j].name, usersInDepartment[j].email, matchingDepartmentsInit[i].department, matchingDepartmentsInit[i].forms);
+                await sendEmailInit(usersInDepartment[j].nickname, String(matchingDepartmentsInit[i].department), String(matchingDepartmentsInit[i].forms), usersInDepartment[j].email);
+            }
+        }
+
+        return new Response('All the emails were send successfully', {status: 200,});
+    } catch (error) {
+        console.error('Error:', error);
+        throw error;
+    }
 }
--- middleware.ts
+++ middleware.ts
@@ -2,15 +2,15 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
-    const jwt = request.cookies.get("myTokenName");
-    if (!jwt) return NextResponse.redirect(new URL("/", request.url));
-    try {
-      return NextResponse.next();
-    } catch (error) {
-      return NextResponse.redirect(new URL("/", request.url));
-    }
+    // const jwt = request.cookies.get("myTokenName");
+    // if (!jwt) return NextResponse.redirect(new URL("/", request.url));
+    // try {
+    //   return NextResponse.next();
+    // } catch (error) {
+    //   return NextResponse.redirect(new URL("/", request.url));
+    // }
 }
 
 export const config = {
-    matcher: ['/home_page/:path*', '/api/((?!users/\\[id\\]).*)'],
+    // matcher: ['/home_page/:path*', '/api/((?!users/\\[id\\]).*)'],
 }
